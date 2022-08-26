4d44058c0b35ea9e32c4de0fa7d97875d5df88be
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-08-26 09:57:18 -0400

    s3 changes

--- s3.js
+++ s3.js
@@ -2,7 +2,7 @@
 // const fs = require('fs')
 // const S3 = require('aws-sdk/clients/s3')
 
-// const bucketName = process.env.AWS_BUCKET_NAME
+// // const bucketName = process.env.AWS_BUCKET_NAME
 // const region = process.env.AWS_ACCESS_KEY
 // const accessKeyId = process.env.AWS_ACCESS_KEY
 // const secretAccessKey = process.env.AWS_SECRET_KEY
@@ -36,16 +36,26 @@ const AWS = require ('aws-sdk')
 // Create S3 service object
 const s3 = new AWS.S3({apiVersion: '2006-03-01'})
 
+//
+
+// const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_ACCESS_KEY
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+
 // Read the file elsewhere, accept it here
 module.exports = function (file) {
     console.log(file.mimetype)
     const uploadParams = {
         Bucket: process.env.BUCKET, 
-        accessKeyId: process.env.AWS_ACCESS_KEY ,
+        // accessKeyId:process.env.AWS_ACCESS_ID,
+        // secretAccessKey: process.env.AWS_SECRET_KEY,
+        // accessKeyId: process.env.AWS_ACCESS_KEY ,
         Key: Date.now() + '_' + file.originalname, 
         Body: file.buffer,
         ACL: 'public-read',
-        ContentType: file.mimetype
+        ContentType: file.mimetype,
     }
 
     return s3.upload(uploadParams).promise()
